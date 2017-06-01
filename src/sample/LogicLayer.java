99a6648a620c7f0f33196aff31bf884e4167c37f
marc-antoine wolters <marc-antoine.wolters@xs4all.nl>
2017-06-01 11:26:50 +0200

    startup
    startup

---
+++ src/sample/LogicLayer.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by Marc-Antoine on 6/1/2017.
+ */
+public class LogicLayer {
+
+
+
+
+
+
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,12 +8,25 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private LogicLayer ll;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+        primaryStage.setTitle("StorJ UI");
+        primaryStage.setScene(new Scene(root, 460, 350));
         primaryStage.show();
+        ll = new LogicLayer();
+
+
+        // try to get connection to storJ (is server online?)
+
+        //get login shit from form. tty to sign in.
+
+        //if (login.succesfull) show 2e form.
+
+        //else show error message.
+
     }
 
 
